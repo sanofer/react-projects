89604cca01d358a9e7727bd758e5be3cb997140c
Halima Sanofer <halimasanofer@gmail.com>
2023-06-05 20:31:23 +0530

    Added functionalities to fetch tour details

---
+++ Tours/setup/src/Tours.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Tour from "./Tour";
+const Tours = ({ tours, removeTour }) => {
+  return (
+    <section>
+      <div className="tours">
+        {tours.map((tour) => {
+          return <Tour key={tour.id} {...tour} removeTour={removeTour}></Tour>;
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Tours;
